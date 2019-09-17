1745fbbcd10e879dd7b579be6eb0e768c0a323b4
Soronkkk <soronk99@gmail.com>
2019-09-17 09:37:50 +0300

    upd pom and main

--- src/main/java/app/webparser/Main.java
+++ src/main/java/app/webparser/Main.java
@@ -1,11 +1,14 @@
 package app.webparser;
 
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         GetHTML getHTML = new GetHTML("https://www.simbirsoft.com");
         getHTML.getHtmlToText();
         TextAnalysis textAnalysis = new TextAnalysis();
         textAnalysis.setSplitWords(getHTML.getPathName());
-        textAnalysis.wordCount();
+        Map<String, Integer> occurrences = textAnalysis.wordCount();
+        textAnalysis.printWords(occurrences);
     }
 }
